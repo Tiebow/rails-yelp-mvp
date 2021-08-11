23783a95cd7b4f841a5707fa0b83c0a88ed468f1
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-11 16:08:54 +0200

    done but ugly

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurants_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurants_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
