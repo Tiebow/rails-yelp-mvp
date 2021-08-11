5c60b80900347d3da56d21ad8c661ad52df53bb6
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-11 16:24:35 +0200

    completed rake without css

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,8 +14,10 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurants_params)
-    @restaurant.save
-    redirect_to restaurants_path
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else render :new
+    end
   end
 
   private
